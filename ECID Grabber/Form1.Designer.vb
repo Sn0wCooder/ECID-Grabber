39cf112968dfd9d66f5ca7a91d1e6b5e410157d0
Leonardo Alfreducci <leoalfreducci@gmail.com>
2015-09-27 20:52:36 +0200

    Initial commit

---
+++ ECID Grabber/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "GET"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(200, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(218, 66)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ECID Grabber"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ ECID Grabber/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Management
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim searcher As New ManagementObjectSearcher("SELECT * FROM  Win32_PnPEntity")
+            Dim regex As New Regex("ECID:#?[0-9A-Fa-f]{16}")
+            Dim str As String = Nothing
+            Dim obj2 As ManagementObject
+            For Each obj2 In searcher.Get
+                If (((Not obj2.Item("Service") Is Nothing) AndAlso obj2.Item("Name").ToString.Contains("Apple")) AndAlso regex.IsMatch(obj2.ToString)) Then
+                    str = regex.Match(obj2.ToString).Value.Split(New Char() {":"c})(1)
+                    If (Not str Is Nothing) Then
+                        TextBox1.Text = str
+
+                    Else
+                        Interaction.MsgBox("Could not find ECID!", MsgBoxStyle.ApplicationModal, Nothing)
+                    End If
+                End If
+            Next
+            If (str Is Nothing) Then
+                Interaction.MsgBox(("Could not find ECID!" & Environment.NewLine & "Is the device plugged into your computer in Recovery Mode?"), MsgBoxStyle.ApplicationModal, Nothing)
+            End If
+        Catch exception1 As Exception
+            'ProjectData.SetProjectError(exception1)
+            Dim exception As Exception = exception1
+            Interaction.MsgBox(exception.Message.ToString, MsgBoxStyle.Exclamation, Nothing)
+            'ProjectData.ClearProjectError()
+        End Try
+    End Sub
+End Class
